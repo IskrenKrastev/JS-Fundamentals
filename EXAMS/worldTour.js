5275bea91613320c957374d001f5cebe01e4df2a
Iskren Krastev <iskren.nexus@gmail.com>
2020-10-28 11:57:45 +0200

    more code to world tour

--- EXAMS/worldTour.js
+++ EXAMS/worldTour.js
@@ -1,4 +1,10 @@
 function worldTour(input) {
 
 }
-worldTour()
+worldTour([
+    'Hawai::Cyprys-Greece',
+    'Add Stop:7:Rome',
+    'Remove Stop:11:16',
+    'Switch:Hawai:Bulgaria',
+    'Travel'
+]);
