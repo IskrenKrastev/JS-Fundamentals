ab9a6cf2b68697d08bd71f6cbb9171fb04c92e97
Iskren Krastev <iskren.nexus@gmail.com>
2020-10-28 14:31:53 +0200

    Create destinationMapper.js

---
+++ EXAMS/destinationMapper.js
@@ -0,0 +1,16 @@
+function destinationMapper(input) {
+
+    let pattern = /([=/])(?<city>[A-Z][a-z]{2,})(\1)/g;
+    let validPlaces = input.matchAll(pattern);
+    let sum = 0;
+    let places = [];
+
+    for (let place of validPlaces) {
+        places.push(place.groups['city']);
+        sum += place.groups['city'].length
+    }
+    console.log(`Destinations: ${places.join(', ')}`);
+    console.log(`Travel Points: ${sum}`);
+}
+destinationMapper('=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=');
+destinationMapper('ThisIs some InvalidInput');
