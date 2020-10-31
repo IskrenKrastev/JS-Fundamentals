92a72f4b6589cdfc35631b10b8784f73d2f6237b
Iskren Krastev <iskren.nexus@gmail.com>
2020-10-31 13:14:54 +0200

    add final code

--- EXAMS/emojiDetector.js
+++ EXAMS/emojiDetector.js
@@ -30,7 +30,6 @@ function emojiDetector(input) {
             }
         }
     }
-
     console.log(`Cool threshold: ${coolness}`);
     if (emojisCount > 0) {
         console.log(`${emojisCount} emojis found in the text. The cool ones are:`);
@@ -38,7 +37,6 @@ function emojiDetector(input) {
             console.log(emoji);
         }
     }
-
 }
 emojiDetector([
     'In the Sofia Zoo there are 311 animals in total! ::Smiley:: This includes 3 **Tigers**, 1 ::Elephant:, 12 **Monk3ys**, a **Gorilla::, 5 ::fox:es: and 21 different types of :Snak::Es::. ::Mooning:: **Shy**'
